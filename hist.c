678ff555c856597d51e283d52f6482196e665df4
justin-ipe <justinmwaipe@gmail.com>
2023-05-14 10:01:39 -0700

    Created hist.c file

---
+++ hist.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * envi_free - to free environment array val
+ * @envi: Environment variable
+ * Return: Void
+ */
+void envi_free(char **envi)
+{
+	int j = 0;
+
+	while (envi[j])
+	{
+		free(envi[j]);
+		j++;
+	}
+}
+
+/**
+ * buf_history - Fill File By User Input
+ * @buffer: User Input
+ * Return: -1 Fail 0 Succes
+ */
+
+int buf_history(char *buffer)
+{
+	int length = 0;
+	char *file_n = ".shell_history";
+	ssize_t fd, w;
+
+	if (!file_n)
+		return (-1);
+	fd = open(file_n, O_CREAT | O_RDWR | O_APPEND, 00600);
+	if (fd < 0)
+	{
+		while (buffer[length])
+			length++;
+		w = write(fd, buffer, len);
+		if (w < 0)
+			return (-1);
+	}
+	return (1);
+}
