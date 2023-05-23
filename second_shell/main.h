57f0cdd40aac605fc0c4198acb0bdf828615bfd1
Dennismwendwa <dennismusemb2@.com>
2023-05-23 18:23:46 +0300

    added more files

---
+++ second_shell/main.h
@@ -0,0 +1,27 @@
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <dirent.h>
+#include <errno.h>
+
+
+/***** ENV *****/
+extern char **environ;
+
+
+
+
+
+
+#endif
